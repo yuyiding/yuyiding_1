a864e513070426067e89f49ecd6803bb3bdda8d8
yuyiding1 <1106428258@qq.com>
2020-04-01 22:24:27 +0800

    code1.py

--- code1.py
+++ code1.py
@@ -12,3 +12,15 @@ n=input()
 n=int(n)
 
 a=float(0)
+
+for i in range(1,n+1):                              
+    q=float(input())
+    if q>=200:
+        q=q-40
+    elif q>=100 and q<200:
+        q=q-15
+    elif q>=50 and q<100:
+        q=q-5
+    else:
+        q=q
+    a=a+q
